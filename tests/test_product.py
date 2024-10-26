d651b458fd5858c43189075852df38e7bd366106
ksenia <venina621@mail.ru>
2024-10-26 20:39:27 +0300

    files ignore

---
+++ tests/test_product.py
@@ -0,0 +1,9 @@
+import pytest
+
+from src.product import Product
+
+def test_product_init(product):
+    assert product.name == "Iphone 15"
+    assert product.description == "512GB, Gray space"
+    assert product.price == 210000.0
+    assert product.quantity == 8
