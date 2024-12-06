c06f4d5de64cd8f9c8f595a7732fd49c5ca801aa
ksenia <venina621@mail.ru>
2024-12-06 14:29:54 +0300

    added changes

--- src/category.py
+++ src/category.py
@@ -12,9 +12,9 @@ class Category:
     category_count = 0
     product_count = 0
 
-    @property
-    def products(self):
-        return self.__products
+    # @property
+    # def products(self):
+    #     return self.__products
 
     def add_product(self, new_product: Product):
         self.__products.append(new_product)
--- tests/test_category.py
+++ tests/test_category.py
@@ -15,18 +15,17 @@ def test_category_tv(category_tv, product_4):
     assert category_tv.name == "Телевизоры"
     assert category_tv.description == ("Современный телевизор, который позволяет наслаждаться просмотром,"
                                        " станет вашим другом и помощником")
-    assert category_tv.products == ('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    assert category_tv.products == '55" QLED 4K, 123000.0 руб. Остаток: 7 шт.\n'
 
 
 def test_category_smart(category_smart, product_1, product_2, product_3):
     assert category_smart.name == "Смартфоны"
     assert category_smart.description == ("Смартфоны, как средство не только коммуникации, "
                                           "но и получения дополнительных функций для удобства жизни")
-    assert category_smart.products == (
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-    )
-    product2 = ("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = ("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    assert category_smart.products == '''Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.
+Iphone 15, 210000.0 руб. Остаток: 8 шт.
+Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n'''
+
 
 
 def test_category(category_smart, product_4):
