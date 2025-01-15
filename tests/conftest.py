1658dabe5f53ff73a74c68d2ec709ee07efe179b
ksenia <venina621@mail.ru>
2025-01-16 00:40:38 +0300

    files ignore,Made changes to the project description

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,29 +5,65 @@ from src.category import Category
 
 
 @pytest.fixture
-def first_category():
-    return Category(
-        name="Смартфоны",
-        description="Смартфоны, как средство не только коммуникации, "
-                    "но и получения дополнительных функций для удобства жизни",
-        products=[
-            Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
-            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
-            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
-        ],
+def product_1():
+    return Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
     )
 
 
 @pytest.fixture
-def second_category():
-    return Category(
-        name="Телевизоры",
-        description="Современный телевизор, который позволяет наслаждаться просмотром,"
-                    " станет вашим другом и помощником",
-        products=[Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)],
-    )
-
-
-@pytest.fixture
-def product():
+def product_2():
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture
+def product_3():
+    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+
+@pytest.fixture
+def product_4():
+    return Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+
+
+@pytest.fixture
+def category_tv(product_4):
+    return Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product_4],
+    )
+
+
+@pytest.fixture
+def category_smart(product_1, product_2, product_3):
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product_1, product_2, product_3],
+    )
+
+
+@pytest.fixture
+def product_str_1(product_1):
+    return f"{product_1.name}, {product_1.price} руб. Остаток: {product_1.quantity} шт."
+
+
+@pytest.fixture
+def product_str_2(product_2):
+    return f"{product_2.name}, {product_2.price} руб. Остаток: {product_2.quantity} шт."
+
+
+@pytest.fixture
+def counter(product_1, product_2):
+    return product_1.price * product_1.quantity + product_2.price * product_2.quantity
+
+
+@pytest.fixture
+def counter_2(product_2, product_3):
+    return product_2.price * product_2.quantity + product_3.price * product_3.quantity
+
+
+@pytest.fixture
+def sum_counter(category_smart):
+    return f"{category_smart.name}, {category_smart.products[0].quantity + category_smart.products[1].quantity + category_smart.products[2].quantity} шт."
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,41 @@
-def test_product_init(product):
-    assert product.name == "Iphone 15"
-    assert product.description == "512GB, Gray space"
-    assert product.price == 210000.0
-    assert product.quantity == 8
+from src.product import Product
+
+new_product = Product.new_product(
+    {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    }
+)
+
+
+def test_samsung(product_1):
+    assert product_1.name == "Samsung Galaxy S23 Ultra"
+    assert product_1.description == "256GB, Серый цвет, 200MP камера"
+    assert product_1.price == 180000.0
+    assert product_1.quantity == 5
+
+
+def test_iphone(product_2):
+    assert product_2.name == "Iphone 15"
+    assert product_2.description == "512GB, Gray space"
+    assert product_2.price == 210000.0
+    assert product_2.quantity == 8
+
+
+def test_new_product():
+    new_product.price = 0
+    assert new_product.price == 180000
+    new_product.price = 12000
+    assert new_product.price == 12000
+
+
+def test_new_str(product_str_1, product_str_2):
+    assert product_str_1 == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    assert product_str_2 == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
+
+
+def test_counter(counter, counter_2, product_1, product_2, product_3):
+    assert product_1 + product_2 == counter
+    assert product_2 + product_3 == counter_2
